1a35cb861f2da91eb5d0f5d1c0e46b16d10f4405
sugaken0528 <susunsun0528@gmail.com>
2022-12-06 01:38:55 +0900

    抽出していない単語が出力されるバグを修正

--- createDataset/classifierData/source/classifier.py
+++ createDataset/classifierData/source/classifier.py
@@ -7,16 +7,15 @@ import os
 
 
 class classifier:
-    def __init__(self, result, docs1, docs, wlist1, TF_list):
+    def __init__(self, result, specification, wordList, TF_list):
         self.result = result
-        self.docs1 = docs1
-        self.docs = docs
-        self.wlist1 = wlist1
+        self.specification = specification
+        self.wordList = wordList
         self.TF_list = TF_list
 
     def createClassifierList(self):
-        wordList = []
-        classList = []
+        wordList = []  # VDM++仕様書に必要な候補の単語を格納
+        classList = []  # クラスの候補となる単語を格納
         for x in self.result.values:
             if x[10] >= 1.0:
                 list_ = []
@@ -32,50 +31,53 @@ class classifier:
         wordList = self.get_unique_list(wordList)
         classList = self.get_unique_list(classList)
 
-        dic = pd.read_csv(
-            "\\Users\\ksk\\sync\\lab\\research\\2021\\GVA3\\Source\\dic.csv")
-        counter_suffix = ['位', '宇', '折', '果', '箇', '荷', '菓', '掛', '顆', '回', '階', '画', '頭', '方', '株', '巻', '管', '缶', '基', '機', '騎', '切', '客', '脚', '行', '局', '句', '軀', '口', '具', '組', '景', '桁', '件', '軒', '個', '戸', '号', '合', '梱', '献', '喉', '座', '棹', '冊', '皿', '氏', '締', '字', '軸', '室', '首', '重', '床', '条', '畳', '錠', '帖', '筋', '食', '隻', '膳', '双',
-                          '艘', '足', '揃', '体', '袋', '台', '題', '立', '卓', '束', '玉', '着', '丁', '挺', '帳', '張', 'つ', '対', '通', '番', '粒', '艇', '滴', '点', '度', '等', '堂', '人', '把', '羽', '張', '刎', '杯', '柱', '鉢', '発', '尾', '匹', '瓶', '振', '部', '幅', '服', '房', '篇', '遍', '本', '間', '枚', '前', '幕', '棟', '名', '面', '門', '問', '山', '葉', '流', '旒', '両', '領', '輪', '連', '椀', '碗']
+        how_to_count_dic = pd.read_csv(
+            "\\Users\\ksk\\sync\\lab\\research\\2021\\GVA3\\Source\\how_to_count_dic.csv")  # 数え方の辞書
+        unit_dic = ['位', '宇', '折', '果', '箇', '荷', '菓', '掛', '顆', '回', '階', '画', '頭', '方', '株', '巻', '管', '缶', '基', '機', '騎', '切', '客', '脚', '行', '局', '句', '軀', '口', '具', '組', '景', '桁', '件', '軒', '個', '戸', '号', '合', '梱', '献', '喉', '座', '棹', '冊', '皿', '氏', '締', '字', '軸', '室', '首', '重', '床', '条', '畳', '錠', '帖', '筋', '食', '隻', '膳', '双',
+                    '艘', '足', '揃', '体', '袋', '台', '題', '立', '卓', '束', '玉', '着', '丁', '挺', '帳', '張', 'つ', '対', '通', '番', '粒', '艇', '滴', '点', '度', '等', '堂', '人', '把', '羽', '張', '刎', '杯', '柱', '鉢', '発', '尾', '匹', '瓶', '振', '部', '幅', '服', '房', '篇', '遍', '本', '間', '枚', '前', '幕', '棟', '名', '面', '門', '問', '山', '葉', '流', '旒', '両', '領', '輪', '連', '椀', '碗']  # 単位の辞書
         v_list = []
 
         value = ""
-        for x in self.docs1:
-            for y in x.split():
-                if (any(map(str.isdigit, y))):  # 数字を含む文章かどうか
-                    for num in y:
+        for sentence in self.specification:
+            for word in sentence.split():
+                if (any(map(str.isdigit, word))):  # 数字を含む単語かどうか
+                    for num in word:
                         if num.isdigit() or num == ".":
                             value = value+num
-                    for z in counter_suffix:
-                        if not len(y) == y.find(value)+len(value):
-                            if z in y[y.find(value)+len(value)]:  # 文章中に単位があるか
-                                for d in dic.values:
-                                    if not (d[2] is np.nan):
-                                        if z in d[2]:
-                                            for y2 in x.split():
-                                                # 単位がある文にその単位を使う名詞があるか
-                                                if d[1] in y2 and not y2[len(y2)-1].isdigit() and not y == y2:
-                                                    list_ = []
-                                                    list_.append("INDEX_ID")
-                                                    list_.append(y2)
-                                                    list_.append(value)
-                                                    v_list.append(list_)
+                    for unit in unit_dic:
+                        nextIndexValue = word.find(
+                            value)+len(value)  # 単語の内数字の次の要素のインデックス
+                        # 数字の次の要素に単位があるか
+                        if len(word) > nextIndexValue and unit in word[nextIndexValue]:
+                            for wordCountSet in how_to_count_dic.values:
+                                # 数え方の辞書に単位が含まれている場合
+                                if not (wordCountSet[2] is np.nan) and unit in wordCountSet[2]:
+                                    for word2 in sentence.split():
+                                        # 単位がある文にその単位を使う名詞があるか
+                                        if wordCountSet[1] in word2 and not word2[len(word2)-1].isdigit() and not word == word2 and self.examine_include_word(word2, wordList):
+                                            list_ = []
+                                            list_.append("INDEX_ID")
+                                            list_.append(word2)
+                                            list_.append(value)
+                                            v_list.append(list_)
                 value = ""
         d_list = []
         for x in v_list:
-            for count_w, y in enumerate(self.wlist1):
+            for count_w, y in enumerate(self.wordList):
                 if y == x[1]:  # 定数名の単語を探す
                     for p, z in enumerate(self.TF_list):
                         if z[count_w] > 0:  # 定数名のTF_IDF値を持つ文を抽出
                             sentence_list = []
                             for count_TF, l in enumerate(z):
                                 if l > 0:
-                                    sentence_list.append(self.wlist1[count_TF])
+                                    sentence_list.append(
+                                        self.wordList[count_TF])
                                 lists_ = []
                                 lists_.append(x[1])
                                 lists_.append(x[2])
                                 lists_.append(z[count_w])
                                 for m in sentence_list:
-                                    for d in counter_suffix:
+                                    for d in unit_dic:
                                         if x[2] in m and d in m:
                                             d_list.append(lists_)
         d_list = self.get_unique_list(d_list)
@@ -133,7 +135,7 @@ class classifier:
         # クラスとインスタンス変数の書き込み
         for i in range(len(instanceList)):
             classWord = classList[i]
-            print(classWord)
+            # print(classWord)
             for x in instanceList[i]:
                 with open("\\Users\\ksk\\sync\\lab\\research\\2021\\GVA3\\Source\\createDataset\\classifierData\\data\\classifier_advance\\"+classWord+".csv", 'w', encoding='utf8') as f:
                     writer = csv.writer(f, lineterminator='\n')
@@ -151,7 +153,13 @@ class classifier:
         seen = []
         return [x for x in seq if x not in seen and not seen.append(x)]
 
-    # 重複するインスタンスを削除
+    def examine_include_word(self, word, wordList):
+        for i in range(len(wordList)):
+            if wordList[i][1] == word:
+                return True
+
+        # 重複するインスタンスを削除
+
     def removeDuplicateInstance(self, classList, classifierList):
         tempList = []
         # print(classifierList)
--- createDataset/classifierData/source/doClassifier.py
+++ createDataset/classifierData/source/doClassifier.py
@@ -6,43 +6,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 df = pd.read_csv(
     "\\Users\\ksk\\sync\\lab\\research\\2021\\GVA3\\Source\\createDataset\\loadTxt\\advance_kyogi_load.txt")
 
-# 変数名を変更中
-# docs1 → specification
-# docs → docsNumber
-
 specification = np.array([])
 for x in df.values:
     specification = np.append(specification, x)
-
-docsNumber = np.array([])  # 数値を含む文章のみを格納するリスト
-for x in df.values:
-    if (any(map(str.isdigit, x[0]))):  # 文章が数値を含む場合
-        docsNumber = np.append(docsNumber, x)
 
 # 文章中の単語にTFI-DF値を付与
 vectorizer = TfidfVectorizer(use_idf=True, token_pattern=u'(?u)\\b\\w+\\b')
 vecs = vectorizer.fit_transform(specification)
 TF_list = vecs.toarray()  # (97,363) 97文章,363単語
 
-words = vectorizer.get_feature_names_out()  # (363,) 単語のみを格納
-
-doc_list = []
-for sentence_id, vec in zip(range(len(specification)), TF_list):  # vec(363,)
-    docs = []
-    count = 0
-    # TFI-DF値を昇順にソート
-    for word_id, tfidf in sorted(enumerate(vec), key=lambda x: x[1], reverse=True):
-        lemma = words[word_id]
-        if tfidf > 0.00:
-            doc = [lemma, tfidf]
-            docs.append(doc)
-            count = count+1
-    doc = [sentence_id, count]
-    docs.insert(0, doc)
-    doc_list.append(docs)
+wordList = vectorizer.get_feature_names_out()  # (363,) 単語のみを格納
 
 result = pd.read_csv(
     "\\Users\\ksk\\sync\\lab\\research\\2021\\GVA3\\Source\\createDataset\\resultData\\result_classifier_advance.csv")
 
-classifier = classifier(result, specification, docs, words, TF_list)
+classifier = classifier(result, specification, wordList, TF_list)
 createClassifierList = classifier.createClassifierList()
