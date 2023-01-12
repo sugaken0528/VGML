e1ef3dedf7ef2b396349bd43ae28d5cb0222bf8c
sugaken0528 <susunsun0528@gmail.com>
2023-01-13 07:35:39 +0900

    正解のVDM生成

--- createDataset/classifierData/source/mecabTest.py
+++ createDataset/classifierData/source/mecabTest.py
@@ -1,6 +1,6 @@
 import MeCab
 m = MeCab.Tagger("-Ochasen")
-nouns = m.parse("カラーブロック有効移動").splitlines()
+nouns = m.parse("同メールアドレス").splitlines()
 for i in range(len(nouns)-1):
     print(nouns[i].split()[0])
     print(nouns[i].split()[1])
