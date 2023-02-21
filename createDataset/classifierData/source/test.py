376af4e522cce63ad1bf20c31211b3d9fd72a770
sugaken0528 <susunsun0528@gmail.com>
2023-02-22 02:30:19 +0900

    不要なファイルを削除

--- createDataset/classifierData/source/test.py
+++
@@ -1,5 +0,0 @@
-classList = [['企業','インターンシップ'], '企業', '企業担当者', '学生', '教員']
-word = ['企業','インターンシップ']
-
-if word in classList:
-    print("yes")
--- study/test/test2.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("wnjpn.db")
-
-cur = conn.execute("select synset1,synset2 from synlink where link='hypo'")
-
-for row in cur:
-    hyper_term = row[0]
-    hypo_term = row[1]
